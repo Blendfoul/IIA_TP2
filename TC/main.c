d768472b05bfcf016aff8e32cc47f92e611b5885
João Rodrigues <joao.duarte353@gmail.com>
2020-01-02 15:53:04 +0000

    Initial commit

---
+++ TC/main.c
@@ -0,0 +1,13 @@
+#include "utils.h"
+
+int main(int argc, char const *argv[])
+{
+    if(argc != 1){
+        printf("Sintaxe: tc.exe <experiencia>");
+        return 1;
+    }
+
+    
+    
+    return 0;
+}
